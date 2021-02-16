14ec36d923519c79ec3260fb361b922efc2e201e
Abdullah Abdulfatah <boomjay008@gmail.com>
2021-02-16 07:52:16 -1200

    Directory error

--- src/homepage.components.jsx
+++ src/homepage.components.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import './'
+import './homepage.styles.scss'
 
 const HomePage = () => (
     <div className='homepage'>
-        <div className = 'directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>HATS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
         <div className = 'directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
