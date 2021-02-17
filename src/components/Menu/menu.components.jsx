24c46512b35e755b9163df292fb76b6346d6dd17
Abdullah Abdulfatah <boomjay008@gmail.com>
2021-02-16 12:01:27 -1200

    directory mods

--- src/components/Menu/menu.components.jsx
+++ src/components/Menu/menu.components.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-
-const MenuItem = ({props})=> (
-    <div>
-        <div className='menu-item'>
+import '../Menu/menu.styles.scss'
+const MenuItem = ({title, imageUrl , size})=> (
+        <div 
+            style={{
+                backgroundImage: `url(${imageUrl})`
+                
+            }}
+            className= {`${size} menu-item`}>
                 <div className='content'>
-                    <h1 className='title'>{}</h1>
-                    <span className='subtitle'>{}</span>
+                    <h1 className='title'>{title}</h1>
+                    <span className='subtitle'>SHOP NOW</span>
                 </div>
-            </div>
-    </div>
+        </div>
+    
 )
 export default MenuItem
